5e1c7cfcce3f21dc5ab4c317c967aaabdf3daa0f
laetitia123 <uwizelaetitia@gmail.com>
2019-09-15 08:33:38 +0200

    unittest has been addedand succesfuly  passed

---
+++ test_articles.py
@@ -0,0 +1,22 @@
+import unittest
+
+from app.models import Articles
+
+class ArticlesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Movie class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_articles = Articles(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993,'ffffffffffff','date')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_articles,Articles))
+
+
+   
+if __name__ == '__main__':
+    unittest.main()     
