1922c98ef6d7b111ff9497042f7275814d2310c4
laetitia123 <uwizelaetitia@gmail.com>
2019-09-15 08:40:21 +0200

    unittest for sources class has been added and succesfuly  passed

--- test_articles.py
+++ test_articles.py
@@ -1,6 +1,6 @@
 import unittest
 
-from app.models import Articles
+from app.models import Articles,Sources
 
 class ArticlesTest(unittest.TestCase):
     '''
@@ -16,7 +16,19 @@ class ArticlesTest(unittest.TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.new_articles,Articles))
 
+class SourcesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Movie class
+    '''
 
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_sources = Sources(1234,'A thrilling new Python Series','/khsjha27hbs',8.5,129993,'ffffffffffff','date')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_sources,Sources))
    
 if __name__ == '__main__':
     unittest.main()     
